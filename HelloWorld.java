286354af54d0de0d6108716215562c9b2049c098
Rashi <rashia15@gmail.com>
2025-04-21 20:03:17 +0530

    Added code so that app listens on 8080

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,26 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello from basic Java app!");
-        System.out.flush(); // important!
-        try {
-            Thread.sleep(60000);
-        }catch(InterruptedException e){
-            e.printStackTrace();
-        }
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
+public class HelloWorld {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started on port 8080");
+    }
+
+    static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Hello from Java on Docker!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
