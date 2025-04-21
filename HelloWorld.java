8d7e5edd58d02805882fdf393bfc649e87ce0340
Rashi <rashia15@gmail.com>
2025-04-21 19:47:46 +0530

    added try catch block

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,11 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from basic Java app!");
         System.out.flush(); // important!
-        Thread.sleep(60000);
+        try {
+            Thread.sleep(60000);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
 
     }
 }
