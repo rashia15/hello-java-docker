4b4b7dfefe6777023420fc93b700f846122c02ad
Rashi <rashia15@gmail.com>
2025-04-21 19:31:20 +0530

    added sleep for 60 seconds

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from basic Java app!");
         System.out.flush(); // important!
+        Thread.sleep(60000);
 
     }
 }
